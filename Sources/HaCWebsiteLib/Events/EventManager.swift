10e6255d0ea7929db7745ee0fe568ea618d83d3f
Charlie <charliecrisp249@gmail.com>
2017-09-17 15:32:57 +0100

    Add EventManager to combine all event types

---
+++ Sources/HaCWebsiteLib/Events/EventManager.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+public struct EventManager {
+    private(set) static var events: [Event] = []
+
+    public static func update() throws{
+        var updatedEvents : [Event] = []
+
+        try! WorkshopManager.update()
+        //TODO: source events from database
+        let workshopEvents : [Event] = WorkshopManager.workshops.map{(workshop: Workshop) -> WorkshopEvent in
+            return WorkshopEvent(called: workshop.title, at: Date(), described: "There's Pizza", coloured: "yellow", basedOn: workshop)
+        }
+        updatedEvents += workshopEvents
+
+        updatedEvents += [HackathonEvent(called: "Game Gig 2017", at: Date(), described: "Fun and Games", coloured: "green", lasting: 3)]
+
+        events = updatedEvents
+    }
+}
