11ccd4a6f702d469ccca47b619e8f9a0d6309c5e
Charlie <charliecrisp249@gmail.com>
2017-09-17 15:27:29 +0100

    Introduce Event protocol

---
+++ Sources/HaCWebsiteLib/Events/Event.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+protocol Event {
+    var title : String {get}
+    var time : Date {get}
+    var eventDescription: String {get} //NOT related to event content (e.g. workshop content)
+    var colour : String {get}
+    var imageURL : String? {get set}
+    var endTime : Date? {get set}
+    var venue : String? {get set} //for room names etc.
+    var facebookLink : String? {get set}
+}
