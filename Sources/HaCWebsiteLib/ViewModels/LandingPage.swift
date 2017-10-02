379745065ecba04a0fcb6d26f21d48df990c88e9
Tom Maenan Read Cutting <tr395@cam.ac.uk>
2017-10-02 21:50:20 +0100

    Update the about section

--- Sources/HaCWebsiteLib/ViewModels/LandingPage.swift
+++ Sources/HaCWebsiteLib/ViewModels/LandingPage.swift
@@ -4,6 +4,12 @@ import HaCTML
 
 struct LandingPage {
   let updates: [PostCard]
+  let youtube_url = "https://www.youtube.com/hackersatcambridge"
+  let facebook_url = "https://www.facebook.com/hackersatcambridge"
+  let github_url = "https://github.com/hackersatcambridge/"
+  let twitter_url = "https://twitter.com/hackersatcam"
+  let medium_url = "https://medium.com/hackers-at-cambridge"
+  let calendar_url = "https://calendar.google.com/calendar/embed?src=10isedeg17ugvrvg73jq9p5gts%40group.calendar.google.com&ctz=Europe/London"
 
   var node: Node {
     return UI.Pages.base(
@@ -22,35 +28,35 @@ struct LandingPage {
             El.P.containing("We build things, and help other people build things. We also have a bunch of fun"),
             El.A[Attr.href => "#", Attr.className => "LandingIntroduction__link"].containing("Find out more"),
             El.Div[Attr.className => "LandingIntroduction__titleIconRow"].containing(
-              El.A[Attr.href => "https://github.com/hackersatcambridge/", Attr.className => "LandingIntroduction__titleLinkIcon"].containing(
+              El.A[Attr.href => github_url, Attr.className => "LandingIntroduction__titleLinkIcon"].containing(
                 El.Img[
                   Attr.className => "LandingIntroduction__titleLinkIconImg",
                   Attr.src => "/static/images/github_icon.svg",
                   Attr.alt => "GitHub"
                 ]
               ),
-              El.A[Attr.href => "https://www.facebook.com/hackersatcambridge", Attr.className => "LandingIntroduction__titleLinkIcon"].containing(
+              El.A[Attr.href => facebook_url, Attr.className => "LandingIntroduction__titleLinkIcon"].containing(
                 El.Img[
                   Attr.className => "LandingIntroduction__titleLinkIconImg",
                   Attr.src => "/static/images/facebook_icon.svg",
                   Attr.alt => "Facebook"
                 ]
               ),
-              El.A[Attr.href => "https://twitter.com/hackersatcam", Attr.className => "LandingIntroduction__titleLinkIcon"].containing(
+              El.A[Attr.href => twitter_url, Attr.className => "LandingIntroduction__titleLinkIcon"].containing(
                 El.Img[
                   Attr.className => "LandingIntroduction__titleLinkIconImg",
                   Attr.src => "/static/images/twitter_icon.svg",
                   Attr.alt => "Twitter"
                 ]
               ),
-              El.A[Attr.href => "https://www.youtube.com/channel/UCNY6ekV9z84ZYL4qUDusTFw", Attr.className => "LandingIntroduction__titleLinkIcon"].containing(
+              El.A[Attr.href => youtube_url, Attr.className => "LandingIntroduction__titleLinkIcon"].containing(
                 El.Img[
                   Attr.className => "LandingIntroduction__titleLinkIconImg",
                   Attr.src => "/static/images/youtube_icon.svg",
                   Attr.alt => "YouTube"
                 ]
               ),
-              El.A[Attr.href => "https://medium.com/hackers-at-cambridge", Attr.className => "LandingIntroduction__titleLinkIcon"].containing(
+              El.A[Attr.href => medium_url, Attr.className => "LandingIntroduction__titleLinkIcon"].containing(
                 El.Img[
                   Attr.className => "LandingIntroduction__titleLinkIconImg",
                   Attr.src => "/static/images/medium_icon.svg",
@@ -85,13 +91,16 @@ struct LandingPage {
             El.H1[Attr.className => "LandingAbout__subtitle Text--sectionHeading"].containing("About"),
             El.H2[Attr.className => "LandingAbout__headline"].containing("Who are Hackers at Cambridge?"),
             El.P.containing(
+              "Don't let the name scare you, aren't the kind of hackers that try our best to get around complex (or simple) security systems! We are simply an enthusiastic group of people who love technology."
+            ),
+            El.P.containing(
               "Are you a student in Cambridge? Do you want to improve your Tech skills? Then you’ve come to the right place!"
             ),
             El.P.containing(
-              "At HaC it’s our mission to empower everyone to build whatever they want. Whether you are a complete beginner, or a seasoned pro we are running events for you."
+              "At HaC it’s our mission to empower everyone to build whatever they want. Whether you are a complete beginner, or a seasoned pro we run events for you."
             ),
             El.P.containing(
-              "Here's a taste of what we get up to:"
+              "Here's a taste of what we get up to..."
             )
           )
         ),
@@ -102,7 +111,9 @@ struct LandingPage {
               "Run by members of the community and open to everyone, our workshops aim to help and encourage you to work on technology side projects. "
             ),
             El.P.containing(
-              "If you've missed one of our workshops and would like to catch up, check out our YouTube page for recordings. "
+              TextNode("If you've missed one of our workshops and would like to catch up, check out our "),
+              El.A[Attr.href => youtube_url].containing("YouTube"),
+              TextNode(" page for recordings. ")
             ),
             El.P.containing(
               "If you would like to help out at one of our workshops, join the demonstrators group here, we'd love to have you! "
@@ -123,9 +134,15 @@ struct LandingPage {
             ]
           ),
           El.Div[Attr.className => "LandingAbout__text"].containing(
-            El.H1[Attr.className => "LandingAbout__headline"].containing("Hackathons"),
+            El.H1[Attr.className => "LandingAbout__headline"].containing("Hackathons and Game Jams"),
             El.P.containing(
-              "At the end of the Michaelmas and Lent terms, our committee puts on themed Hackathons which allow you to let loose after a term of hard work!"
+              "At the end of the Michaelmas we run the annual Hackers at Cambridge Game Gig - a fun and friendly competition where 100 Cambridge students from a wide variety of courses get together to make games in 12 hours"
+            ),
+            El.P.containing(
+              "At the end of the Lent we additionally run an annual themed hackathon. Last year we partnered with charities in order to tackle problems around the world."
+            ),
+            El.P.containing(
+              "These competitions naturally come with lots of awesome free food."
             )
           )
         ),
@@ -139,7 +156,9 @@ struct LandingPage {
               "We’ll bring the food, you bring the work and if you get stuck, there’s bound to be someone in the room who can help you out!"
             ),
             El.P.containing(
-              "Find us on our Facebook Page to stay up to date."
+              TextNode("Find us on our "),
+              El.A[Attr.href => facebook_url].containing("Facebook"),
+              TextNode(" page to stay up to date.")
             )
           ),
           El.Div[Attr.className => "LandingAbout__imageContainer"].containing(
