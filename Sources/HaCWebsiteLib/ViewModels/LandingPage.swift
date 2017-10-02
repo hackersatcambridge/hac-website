136a754f8ddbd7f044f88b9cd7b6fa28cd313d75
Tom Maenan Read Cutting <tr395@cam.ac.uk>
2017-10-03 00:11:44 +0100

    Make find out more link to about section

--- Sources/HaCWebsiteLib/ViewModels/LandingPage.swift
+++ Sources/HaCWebsiteLib/ViewModels/LandingPage.swift
@@ -25,8 +25,8 @@ struct LandingPage {
               ],
               El.Div[Attr.className => "LandingIntroduction__tagLine"].containing("Cambridge's student tech society")
             ),
-            El.P.containing("We build things, and help other people build things. We also have a bunch of fun"),
-            El.A[Attr.href => "#", Attr.className => "LandingIntroduction__link"].containing("Find out more"),
+            El.P.containing("We build things, and help other people build things. We also have a bunch of fun."),
+            El.A[Attr.href => "#about", Attr.className => "LandingIntroduction__link"].containing("Find out more"),
             El.Div[Attr.className => "LandingIntroduction__titleIconRow"].containing(
               El.A[Attr.href => githubUrl, Attr.className => "LandingIntroduction__titleLinkIcon"].containing(
                 El.Img[
@@ -81,7 +81,7 @@ struct LandingPage {
             }
           )
         ),
-        El.Article[Attr.className => "LandingAbout"].containing(
+        El.Article[Attr.className => "LandingAbout", Attr.id => "about"].containing(
           El.Div[Attr.className => "LandingAbout__imageContainer"].containing(
             El.Img[
               Attr.className => "LandingAbout__image",
