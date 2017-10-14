2941e823c6d37405d411b6123c1923d92aae7c38
Charlie <charliecrisp249@gmail.com>
2017-10-14 18:06:46 +0100

    Reorder images to display correctly on mobile

--- Sources/HaCWebsiteLib/ViewModels/LandingPage.swift
+++ Sources/HaCWebsiteLib/ViewModels/LandingPage.swift
@@ -85,94 +85,96 @@ struct LandingPage {
         //   )
         // ),
         El.Article[Attr.className => "LandingAbout", Attr.id => "about"].containing(
-          El.Div[Attr.className => "LandingAbout__imageContainer"].containing(
-            El.Img[
-              Attr.className => "LandingAbout__image",
-              Attr.src => "/static/images/whoishac.jpg"
-            ]
-          ),
-          El.Div[Attr.className => "LandingAbout__text"].containing(
-            El.H1[Attr.className => "LandingAbout__subtitle Text--sectionHeading"].containing("About"),
-            El.H2[Attr.className => "LandingAbout__headline"].containing("Who are Hackers at Cambridge?"),
-            El.P.containing(
-              "Don't let the name scare you, aren't the kind of hackers that try our best to get around complex (or simple) security systems! We are simply an enthusiastic group of people who love technology."
+          El.Div[Attr.className => "LandingAbout__section"].containing(
+            El.Div[Attr.className => "LandingAbout__imageContainer"].containing(
+              El.Img[
+                Attr.className => "LandingAbout__image",
+                Attr.src => "/static/images/whoishac.jpg"
+              ]
             ),
-            El.P.containing(
-              "Are you a student in Cambridge? Do you want to improve your Tech skills? Then you’ve come to the right place!"
-            ),
-            El.P.containing(
-              "At HaC it’s our mission to empower everyone to build whatever they want. Whether you are a complete beginner, or a seasoned pro we run events for you."
-            ),
-            El.P.containing(
-              "Here's a taste of what we get up to..."
-            )
-          )
-        ),
-        El.Article[Attr.className => "LandingAbout"].containing(
-          El.Div[Attr.className => "LandingAbout__text"].containing(
-            El.H1[Attr.className => "LandingAbout__headline"].containing("Workshops"),
-            El.P.containing(
-              "Run by members of the community and open to everyone, our workshops aim to help and encourage you to work on technology side projects. "
-            ),
-            El.P.containing(
-              TextNode("If you've missed one of our workshops and would like to catch up, check out our "),
-              El.A[Attr.href => youtubeUrl].containing("YouTube"),
-              TextNode(" page for recordings. ")
-            ),
-            El.P.containing(
-              "If you would like to help out at one of our workshops, join the demonstrators group here, we'd love to have you! "
+            El.Div[Attr.className => "LandingAbout__text"].containing(
+              El.H1[Attr.className => "LandingAbout__subtitle Text--sectionHeading"].containing("About"),
+              El.H2[Attr.className => "LandingAbout__headline"].containing("Who are Hackers at Cambridge?"),
+              El.P.containing(
+                "Don't let the name scare you, aren't the kind of hackers that try our best to get around complex (or simple) security systems! We are simply an enthusiastic group of people who love technology."
+              ),
+              El.P.containing(
+                "Are you a student in Cambridge? Do you want to improve your Tech skills? Then you’ve come to the right place!"
+              ),
+              El.P.containing(
+                "At HaC it’s our mission to empower everyone to build whatever they want. Whether you are a complete beginner, or a seasoned pro we run events for you."
+              ),
+              El.P.containing(
+                "Here's a taste of what we get up to..."
+              )
             )
           ),
-          El.Div[Attr.className => "LandingAbout__imageContainer"].containing(
-            El.Img[
-              Attr.className => "LandingAbout__image",
-              Attr.src => "/static/images/hacworkshop.jpg"
-            ]
-          )
-        ),
-        El.Article[Attr.className => "LandingAbout"].containing(
-          El.Div[Attr.className => "LandingAbout__imageContainer"].containing(
-            El.Img[
-              Attr.className => "LandingAbout__image",
-              Attr.src => "/static/images/hachackathon.jpg"
-            ]
-          ),
-          El.Div[Attr.className => "LandingAbout__text"].containing(
-            El.H1[Attr.className => "LandingAbout__headline"].containing("Hackathons and Game Jams"),
-            El.P.containing(
-              "At the end of the Michaelmas we run the annual Hackers at Cambridge Game Gig - a fun and friendly competition where 100 Cambridge students from a wide variety of courses get together to make games in 12 hours"
+          El.Article[Attr.className => "LandingAbout__section"].containing(
+            El.Div[Attr.className => "LandingAbout__imageContainer"].containing(
+              El.Img[
+                Attr.className => "LandingAbout__image",
+                Attr.src => "/static/images/hacworkshop.jpg"
+              ]
             ),
-            El.P.containing(
-              "At the end of the Lent we additionally run an annual themed hackathon. Last year we partnered with charities in order to tackle problems around the world."
-            ),
-            El.P.containing(
-              "These competitions naturally come with lots of awesome free food."
-            )
-          )
-        ),
-        El.Article[Attr.className => "LandingAbout"].containing(
-          El.Div[Attr.className => "LandingAbout__text"].containing(
-            El.H1[Attr.className => "LandingAbout__headline"].containing("HaC Nights"),
-            El.P.containing(
-              "HaC Nights are weekly events we run for people of all experience levels! If you have something you’re working on - be it a project, some supervision work or coursework – then a HaC night is a great social environment for getting things done."
-            ),
-            El.P.containing(
-              "We’ll bring the food, you bring the work and if you get stuck, there’s bound to be someone in the room who can help you out!"
-            ),
-            El.P.containing(
-              TextNode("Find us on our "),
-              El.A[Attr.href => facebookUrl
-          ].containing("Facebook"),
-              TextNode(" page to stay up to date.")
+            El.Div[Attr.className => "LandingAbout__text"].containing(
+              El.H1[Attr.className => "LandingAbout__headline"].containing("Workshops"),
+              El.P.containing(
+                "Run by members of the community and open to everyone, our workshops aim to help and encourage you to work on technology side projects. "
+              ),
+              El.P.containing(
+                TextNode("If you've missed one of our workshops and would like to catch up, check out our "),
+                El.A[Attr.href => youtubeUrl].containing("YouTube"),
+                TextNode(" page for recordings. ")
+              ),
+              El.P.containing(
+                "If you would like to help out at one of our workshops, join the demonstrators group here, we'd love to have you! "
+              )
             )
           ),
-          El.Div[Attr.className => "LandingAbout__imageContainer"].containing(
-            El.Img[
-              Attr.className => "LandingAbout__image",
-              Attr.src => "/static/images/whoishac.jpg"
-            ]
+          El.Article[Attr.className => "LandingAbout__section"].containing(
+            El.Div[Attr.className => "LandingAbout__imageContainer"].containing(
+              El.Img[
+                Attr.className => "LandingAbout__image",
+                Attr.src => "/static/images/hachackathon.jpg"
+              ]
+            ),
+            El.Div[Attr.className => "LandingAbout__text"].containing(
+              El.H1[Attr.className => "LandingAbout__headline"].containing("Hackathons and Game Jams"),
+              El.P.containing(
+                "At the end of the Michaelmas we run the annual Hackers at Cambridge Game Gig - a fun and friendly competition where 100 Cambridge students from a wide variety of courses get together to make games in 12 hours"
+              ),
+              El.P.containing(
+                "At the end of the Lent we additionally run an annual themed hackathon. Last year we partnered with charities in order to tackle problems around the world."
+              ),
+              El.P.containing(
+                "These competitions naturally come with lots of awesome free food."
+              )
+            )
+          ),
+          El.Article[Attr.className => "LandingAbout__section"].containing(
+            El.Div[Attr.className => "LandingAbout__imageContainer"].containing(
+              El.Img[
+                Attr.className => "LandingAbout__image",
+                Attr.src => "/static/images/whoishac.jpg"
+              ]
+            ),
+            El.Div[Attr.className => "LandingAbout__text"].containing(
+              El.H1[Attr.className => "LandingAbout__headline"].containing("HaC Nights"),
+              El.P.containing(
+                "HaC Nights are weekly events we run for people of all experience levels! If you have something you’re working on - be it a project, some supervision work or coursework – then a HaC night is a great social environment for getting things done."
+              ),
+              El.P.containing(
+                "We’ll bring the food, you bring the work and if you get stuck, there’s bound to be someone in the room who can help you out!"
+              ),
+              El.P.containing(
+                TextNode("Find us on our "),
+                El.A[Attr.href => facebookUrl
+            ].containing("Facebook"),
+                TextNode(" page to stay up to date.")
+              )
+            )
           )
-        )
+        ) 
       )
     )
   }
