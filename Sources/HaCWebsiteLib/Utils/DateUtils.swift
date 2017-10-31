2dfa8e4b4faff1f185197535248a6f16009c4b5e
Jared Khan <jaredkhan@me.com>
2017-10-31 01:02:12 +0000

    Avoid implicitly unwrapped optionals

--- Sources/HaCWebsiteLib/Utils/DateUtils.swift
+++ Sources/HaCWebsiteLib/Utils/DateUtils.swift
@@ -14,10 +14,14 @@ enum DateUtils {
 }
 
 extension Date {
-  static func from(year: Int, month: Int, day: Int, hour: Int, minute: Int, timezone: TimeZone? = nil) -> Date! {
+  static func from(year: Int, month: Int, day: Int, hour: Int, minute: Int, timezone: TimeZone? = nil) -> Date {
     let formatter = DateFormatter()
     formatter.dateFormat = "yyyy-MM-dd HH:mm"
     formatter.timeZone = timezone ?? TimeZone(identifier: "Europe/London")
-    return formatter.date(from: "\(year)-\(month)-\(day) \(hour):\(minute)")
+    if let result = formatter.date(from: "\(year)-\(month)-\(day) \(hour):\(minute)") {
+      return result
+    } else {
+      fatalError("Improper date given: \(year)-\(month)-\(day) \(hour):\(minute) does not exist")
+    }
   }
 }
--- Tests/HaCWebsiteLibTests/DateUtilsTests.swift
+++ Tests/HaCWebsiteLibTests/DateUtilsTests.swift
@@ -18,19 +18,10 @@ class DateUtilsTests: HaCWebsiteLibTestCase {
     XCTAssertEqual(twiceOccuringDate, timestampDate)
   }
 
-  func testNeverOccurringDate() {
-    // This time never occurs because the clocks go forwards over it
-    // As such, this date should never have been used. We should crash
-    // (implicitly unwrapped optional shold be nil)
-    let neverOccuringDate = Date.from(year: 2017, month: 03, day: 26, hour: 01, minute: 30)
-    XCTAssertNil(neverOccuringDate)
-  }
-
   static var allTests : [(String, (DateUtilsTests) -> () -> Void)] {
     return [
       ("testDate", testDate),
-      ("testTwiceOccuringDate", testTwiceOccurringDate),
-      ("testNeverOccuringDate", testNeverOccurringDate)
+      ("testTwiceOccuringDate", testTwiceOccurringDate)
     ]
   }
 }
