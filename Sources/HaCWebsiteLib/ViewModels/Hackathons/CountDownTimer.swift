d675e1f17f143af23df4a714161ff67e41e9b137
Tom Read Cutting <tr395@cam.ac.uk>
2017-11-06 20:27:20 +0000

    Implement JavaScript templating in Script.swift
    CountDownTimer now uses that instead of inline JavaScript in string
    literals.

--- Sources/HaCWebsiteLib/ViewModels/Hackathons/CountDownTimer.swift
+++ Sources/HaCWebsiteLib/ViewModels/Hackathons/CountDownTimer.swift
@@ -2,9 +2,10 @@ import HaCTML
 import Foundation
 
 struct CountDownTimer : Nodeable {
-  //let id = "CountDownTimer\(UUID().description)"
-  let id = "CountDownTimer-HARDCODED_ID"
-  let preId = "CountDownTimer-HARDCODED_PRE_ID"
+  let id = "CountDownTimer\(UUID().description)"
+  let preId = "CountDownTimerPre\(UUID().description)"
+  let startDate = Date.from(year: 2017, month: 12, day: 1, hour: 10, minute: 30)
+  let endDate = Date.from(year: 2017, month: 12, day: 1, hour: 20, minute: 0)
   var node: Node {
     return Fragment(
       El.Div[
@@ -14,13 +15,10 @@ struct CountDownTimer : Nodeable {
       El.Div[
         Attr.className => "CountDownTimer",
         Attr.id => id
-      ].containing("Time's up! PLACEHOLDER"),
-      El.Script.containing(
-        //Script("CountDownTimer.js", escapes: [startDate: startDate, id : id, preId: preId])
-        TextNode(
-          "function updateCountDownTimer() { const date_start = new Date(2017,11,2,10,30,0,0); const date_end = new Date(2017,11,2,20,0,0,0); const CountDownTimerId = \"CountDownTimer-HARDCODED_ID\"; const CountDownTimerPreId = \"CountDownTimer-HARDCODED_PRE_ID\"; const current = new Date(); let timeLeft = 0; if(current < date_start) { document.getElementById(CountDownTimerPreId).innerHTML = \"Time left to start\"; timeLeft = date_start.getTime() - current.getTime(); } else { document.getElementById(CountDownTimerPreId).innerHTML = \"Time remaining\"; timeLeft = date_end.getTime() - current.getTime(); } if(timeLeft < 0) { document.getElementById(CountDownTimerId).innerHTML = \"Time's up!\"; return; } let hours = Math.floor(timeLeft / (1000*60*60)); let mins = Math.floor(timeLeft/(1000 * 60) - hours * 60); let secs = Math.floor(timeLeft/1000 - mins * 60 - hours * 60 * 60); document.getElementById(CountDownTimerId).innerHTML = hours + \":\" + (mins<10?\"0\":\"\") + mins + \"<span id=\\\"seconds\\\">:\" + (secs<10?\"0\":\"\") + secs +\"</span>\"; } updateCountDownTimer(); setInterval(updateCountDownTimer,1000);",
-          escapeLevel: .unsafeRaw
-        )
+      ].containing("YOU SHOULD SEE THE TIME REMAINING HERE"),
+      Script(
+        file: "Hackathons/CountDownTimer.js",
+        escapes: ["id": id, "preId": preId, "startDate": startDate, "endDate": endDate]
       )
     )
   }
--- Sources/HaCWebsiteLib/ViewModels/Hackathons/Script.swift
+++ Sources/HaCWebsiteLib/ViewModels/Hackathons/Script.swift
@@ -1,4 +1,40 @@
 import HaCTML
+import DotEnv
+import Foundation
+
+protocol JavaScriptable {
+  var javaScript: String {get}
+}
+
+extension String: JavaScriptable {
+  var javaScript : String {
+    // PROPERLY ESCAPE THIS! (eg. conver newlines to backslash)
+    return "\"\(self)\""
+  }
+}
+
+extension Date: JavaScriptable {
+  var javaScript : String {
+    let calendar = Calendar.current
+    let year = calendar.component(.year, from: self)
+    let month = calendar.component(.month, from: self)
+    let day = calendar.component(.day, from: self)
+    let hour = calendar.component(.hour, from: self)
+    let minute = calendar.component(.minute, from: self)
+    let second = calendar.component(.second, from: self)
+    let milisecond = 0
+    // TODO: work out how JavaScript dates work
+    // TODO: handle timezones
+    return "new Date(\(year),\(month)-1,\(day),\(hour),\(minute),\(second),\(milisecond))"
+  }
+}
+
+struct UnsafeRawJavaScript {
+   let rawScript : String
+   var javaScript : String {
+     return rawScript
+   }
+}
 
 /**
  * This class is used in order load front-end scripts from a file relative to the current path for browsing pleasure.
@@ -7,8 +43,19 @@ import HaCTML
  */
 struct Script : Nodeable {
   let file : String
-  let escapes : [String: String]
+  let escapes : [String: JavaScriptable]
+  let directory : String = DotEnv.get("BACKEND_JS_DIR")!
   var node: Node {
-    return El.Script.containing(TextNode("Some JavaScript", escapeLevel: .unsafeRaw))
+    let pathToFile = directory + "/" + file
+    do {
+      var script = try String(contentsOfFile: pathToFile, encoding: .utf8)
+      escapes.map({ key, value in
+        script = script.replacingOccurrences(of: "{{\(key)}}", with: "\(value.javaScript)")
+      })
+      return El.Script.containing(TextNode(script, escapeLevel: .unsafeRaw))
+    } catch {
+      // TODO: find a way to do this
+      return El.Script.containing(TextNode("console.log(\"failed to load \(pathToFile)\");", escapeLevel: .unsafeRaw))
+    }
   }
 }
