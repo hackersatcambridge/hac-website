7aa6208aeaf22e81c0ff1f9ce15ac936c9de9934
Tom Maenan Read Cutting <tr395@cam.ac.uk>
2017-11-07 00:23:15 +0000

    Cleanup Hackathon protocol

--- Sources/HaCWebsiteLib/ViewModels/Hackathons/Hackathon.swift
+++ Sources/HaCWebsiteLib/ViewModels/Hackathons/Hackathon.swift
@@ -1,14 +1,5 @@
 import HaCTML
 
-/**
-  Anything in an HTML tree
- */
 public protocol Hackathon: Nodeable {
-  // This is very intentionally not an enum.
-  //
-  // Firstly, having something be simultaneously a Node and a concrete
-  // implementation of one is very useful.
-  //
-  // Secondly, this potentially opens us up to extension by other things implementing this.
-  // We should tread that territory carefully.
+
 }
