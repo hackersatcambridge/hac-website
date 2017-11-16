0c1f6c03dcb77669d9b15a687b0e5800a43d549a
Tom Maenan Read Cutting <readcuttingt@gmail.com>
2017-11-16 23:52:51 +0000

    Make section on rules use multi-line strings and Markdown

--- Sources/HaCWebsiteLib/ViewModels/Hackathons/GameGig2017/GameGig2017.swift
+++ Sources/HaCWebsiteLib/ViewModels/Hackathons/GameGig2017/GameGig2017.swift
@@ -64,19 +64,48 @@ struct GameGig2017: Hackathon {
 
   // TODO: grab the rulse from the constitution Repo
   func Rules() -> Nodeable {
-    return Fragment(
-      El.P.containing("The aim of the Game Gig is to create a fun, exciting, original game from scratch in less than 12 hours. We've created a few simple rules to help the process go smoothly for everyone."),
-      El.Ul.containing(
-        El.Li.containing("Please respect and look after the Computer Lab. Note that no food or drinks are permitted in the Intel Laboratory, and we will be checking"),
-        El.Li.containing("You can work on your game in a team of up to four people."),
-        El.Li.containing("Game-making commences at 10:30 and finishes at 20:00."),
-        El.Li.containing("You are free to do whatever you like with your game after the Game Gig. You own the copyright to all the material you create during the Game Gig."),
-        El.Li.containing("You are free to use any tools or libraries available to you to create your game. You can start with any pre-existing code or content that you like and you are free to use third-party assets, as long as you let the judges know what you created yourself and what you didn't. Failure to do so could risk disqualification."),
-        El.Li.containing("It's your responsibility to make sure that you have the right to use third-party assets (for example, that they are public domain or available under an appropriate license)."),
-        El.Li.containing(
-          Markdown("Be kind and considerate to your fellow hackers and our volunteers. We're all here to have fun! By participating in the hackathon, you agree to abide by this [Code of Conduct](https://hackcodeofconduct.org/).")
-        )
-      )
+    return Markdown("""
+      The aim of the Hackers at Cambridge Game Gig 3000 is to create a fun,
+      exciting, original game from scratch in less than 12 hours.
+      We've created a few simple rules to help the process go smoothly for
+      everyone.
+
+      - Please respect and look after the Intel Lab, FW11 and the Computer Lab.
+        Note that food or drink may only be consumed in FW11, and we will be
+        checking!
+
+      - Also not that Computer Science applicants for the University of
+        Cambridge are taking CSAT applications in LT1, so please be quite when
+        moving between rooms, and don't make too much noise.
+
+      - You can work on your game in a team of up to four people.
+
+      - Game-making commences at 10:30 and finishes at 20:00.
+
+      - You are free to do whatever you like with your game after the Game Gig.
+        You own the copyright to all the material you create during the event.
+
+      - You are free to use any tools or libraries available to you to create
+        your game. You can start with any pre-existing code or content that you
+        like and you are free to use third-party assets, as long as you let the
+        judges know what you created yourself and what you didn't. Failure to do
+        so could risk disqualification.
+
+      - It's your responsibility to make sure that you have the right to use
+        third-party assets (for example, that they are public domain or
+        available under an appropriate license).
+
+      - We will be taking lots of photos throughout the event, and publishing
+        them. You and your content may appear on some of these photos. We aim to
+        publish photos that show everyone at their best, but there might be a
+        few that you don’t like. If you see a photo like this, let us know so
+        that we can try to take it down - but be aware that it is difficult to
+        completely erase a photo from all media channels.
+
+      - Be kind and considerate to your fellow hackers and our volunteers.
+        We're all here to have fun! By participating in the hackathon, you agree
+        to abide by this [Code of Conduct](https://hackcodeofconduct.org/).
+      """
     )
   }
 
