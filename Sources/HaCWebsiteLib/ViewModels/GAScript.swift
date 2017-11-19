41d820541add5c83a16bfd100fe1c252340af0ad
Martin Hartt <martinhartt1@gmail.com>
2017-11-19 10:08:21 +0000

    Extract google analytics id to variable

--- Sources/HaCWebsiteLib/ViewModels/GAScript.swift
+++ Sources/HaCWebsiteLib/ViewModels/GAScript.swift
@@ -3,17 +3,18 @@ import Foundation
 import DotEnv
 
 struct GAScript: Nodeable {
+  let googleAnalyticsId = DotEnv.get("GOOGLE_ANALYTICS_ID")!
   var node: Node {
     return TextNode(
       """
       <!-- Global site tag (gtag.js) - Google Analytics -->
-      <script async src=\"https://www.googletagmanager.com/gtag/js?id=\(DotEnv.get("GOOGLE_ANALYTICS_ID")!)\"></script>
+      <script async src=\"https://www.googletagmanager.com/gtag/js?id=\(googleAnalyticsId)\"></script>
       <script>
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
 
-        gtag('config', '\(DotEnv.get("GOOGLE_ANALYTICS_ID")!)');
+        gtag('config', '\(googleAnalyticsId)');
       </script>
       """
     , escapeLevel: .unsafeRaw)
