712de1f716a600096b8694b1f8ec3a27cdb60e46
Charlie Crisp <charliecrisp249@gmail.com>
2017-12-05 15:29:09 +0000

    Source all events from database

--- Sources/HaCWebsiteLib/Events/EventServer.swift
+++ Sources/HaCWebsiteLib/Events/EventServer.swift
@@ -1,23 +1,12 @@
 import Foundation
 
 public struct EventServer {
-
+  //TODO: Rename this to getAllEvents
   static func getEvents() -> [GeneralEvent] {
-    //TODO: source events from database
-    let hypeDuration : Double = 4000000
-    let duration : Double = 4000
-    let workshopEvents : [GeneralEvent] = WorkshopManager.workshops.map{
-      (workshop: Workshop) -> GeneralEvent in
-        return GeneralEvent(title: workshop.title, time:  DateInterval(start: Date(), duration: duration),
-          tagLine: "There's Pizza", color: "purple", hypePeriod: DateInterval(start: Date(), duration: hypeDuration),
-          tags: ["tag1", "tag2"], description: Markdown("Thing"))
-    }.filter{ event in 
-      event.shouldShowAsUpdate
+    let events = try? GeneralEvent.makeQuery().filter("title", .notEquals, nil).all()
+    guard let notNilEvents = events else {
+      return []
     }
-    let externalEvent = GeneralEvent(title: "Game Gig 2017", time: DateInterval(start: Date(), duration: duration),
-      tagLine: "Fun and Games", color: "black", hypePeriod: DateInterval(start: Date(), duration: hypeDuration),
-      tags: [], description: Markdown("Thing"))
-    let externalEvents = [externalEvent]
-    return workshopEvents + externalEvents
+    return notNilEvents
   }
 }
