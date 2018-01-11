f64b951a354753ef0ca9f1c0b1288f858622defe
Jared Khan <jaredkhan@me.com>
2018-01-11 12:16:45 +0000

    Add link to site logo

--- Sources/HaCWebsiteLib/Views/BackBar.swift
+++ Sources/HaCWebsiteLib/Views/BackBar.swift
@@ -11,11 +11,18 @@ struct BackBar: Nodeable {
           "Home"
         )
       ),
-      El.Img[
-        Attr.className => "BackBar__branding SiteLogo SiteLogo--small",
-        Attr.src => Assets.publicPath("/images/hac-logo-dark.svg"),
-        Attr.alt => "Hackers at Cambridge"
-      ]
+      El.A[Attr.href => "/", Attr.className => "BackBar__branding"].containing(
+        El.Img[
+          Attr.className => "SiteLogo SiteLogo--small BackBar__branding__full",
+          Attr.src => Assets.publicPath("/images/hac-logo-dark.svg"),
+          Attr.alt => "Hackers at Cambridge"
+        ],
+        El.Img[
+          Attr.className => "SiteLogo SiteLogo--small SiteLogo--short BackBar__branding__short",
+          Attr.src => Assets.publicPath("/images/hac-logo-small-dark.svg"),
+          Attr.alt => "Hackers at Cambridge"
+        ]
+      )
     )
   }
 }
