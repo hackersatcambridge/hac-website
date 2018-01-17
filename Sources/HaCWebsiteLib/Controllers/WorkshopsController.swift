ea75a4801952541e2350b9a1c1cf52af868581f5
Jared Khan <jaredkhan@me.com>
2018-01-17 14:57:54 +0000

    Remove workshop event schedule, add archive

--- Sources/HaCWebsiteLib/Controllers/WorkshopsController.swift
+++ Sources/HaCWebsiteLib/Controllers/WorkshopsController.swift
@@ -2,45 +2,10 @@ import Kitura
 
 struct WorkshopsController {
 
-  static let upcomingWorkshopCards = [
-    PostCard(
-      title: "Intro to Python",
-      category: .workshop,
-      description: "Snakes are dangerous",
-      backgroundColor: "#852503",
-      imageURL: "/static/images/functions_frame.png"
-    ),
-    PostCard(
-      title: "Intermediate Swift",
-      category: .workshop,
-      description: "Take your entire life to the next level",
-      backgroundColor: "green",
-      imageURL: "/static/images/workshop.jpg"
-    )
-  ]
-
-  static let previousWorkshopCards = [
-    PostCard(
-      title: "Intermediate Swift",
-      category: .workshop,
-      description: "Take your entire life to the next level",
-      backgroundColor: "#852503",
-      imageURL: "/static/images/functions_frame.png"
-    ),
-    PostCard(
-      title: "Binary Exploitation",
-      category: .workshop,
-      description: "Learn all the things.",
-      backgroundColor: "green",
-      imageURL: "/static/images/workshop.jpg"
-    )
-  ]
-
   static var handler: RouterHandler = { request, response, next in
     try response.send(
       WorkshopsIndexPage(
-        upcomingWorkshops: upcomingWorkshopCards,
-        previousWorkshops: previousWorkshopCards
+        allWorkshops: Array(NewWorkshopManager.workshops.values).sorted { $0.title < $1.title }
       ).node.render()
     ).end()
   }
