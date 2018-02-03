00187184e7183cdcd520eb4b42f3e568bcaf34fb
Tom Maenan Read Cutting <readcuttingt@gmail.com>
2018-02-03 16:38:03 +0000

    Fix the day on which the workshop occurs

--- Sources/HaCWebsiteLib/Views/LandingPage/LandingFeatures/ContinuousIntegrationWorkshop.swift
+++ Sources/HaCWebsiteLib/Views/LandingPage/LandingFeatures/ContinuousIntegrationWorkshop.swift
@@ -5,7 +5,7 @@ extension LandingFeatures {
   static var continuousIntegrationWorkshop: LandingFeature {
     return EventFeature(
       eventPeriod: DateInterval(
-        start: Date.from(year: 2018, month: 2, day: 15, hour: 13, minute: 15    ),
+        start: Date.from(year: 2018, month: 2, day: 5, hour: 13, minute: 15    ),
         duration: 1.5 * 60 * 60
       ),
       eventLink: "https://www.facebook.com/events/181764052556196/",
