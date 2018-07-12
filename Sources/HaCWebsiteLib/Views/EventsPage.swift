7913e950e5c1e2e49c59b379095fc7400595d35a
Charlie Crisp <charliecrisp249@gmail.com>
2018-07-12 15:59:35 +0100

    Add basic functionality to iterate through events in database

--- Sources/HaCWebsiteLib/Views/EventsPage.swift
+++ Sources/HaCWebsiteLib/Views/EventsPage.swift
@@ -1,14 +1,26 @@
 import HaCTML
 
 // swiftlint:disable line_length
+func getEventListItem(_ event: GeneralEvent) -> Node {
+  return El.Div[].containing("Node")
+}
 
 struct EventsPage: Nodeable {
 
   var node: Node {
+
+    let events = EventServer.getAllEvents()
+    let eventListItems = events.map({
+      (event: GeneralEvent) -> Node in getEventListItem(event)}
+    )
+    let eventsNoText = "There are " + String(eventListItems.count) + " items in the database"
     return Page(
       title: "Events",
       content: Fragment(
-        El.Div[Attr.className => "LandingTop"].containing("hello")
+        El.Div[].containing([
+          El.Div[].containing(eventsNoText),
+          El.Div.containing(eventListItems)
+        ])
       )
     ).node
   }
