ce7ce416fff8d061618d430fd9157ed2dd309aea
Charlie Crisp <charliecrisp249@gmail.com>
2018-07-12 18:31:29 +0100

    Display all remaining fields in event portal

--- Sources/HaCWebsiteLib/Views/EventsPage.swift
+++ Sources/HaCWebsiteLib/Views/EventsPage.swift
@@ -3,29 +3,44 @@ import HaCTML
 // swiftlint:disable line_length
 func getEventListItem(_ event: GeneralEvent) -> Node {
   let titleText = "Title: " + event.title
-  //TODO: let timetext = event.time
+  let timeText = "Time: " + event.time.debugDescription
   let tagLineText = "Tagline: " + event.tagLine
   let colorText = "Color: " + event.color
-  //TODO: let hypePeriodText = event.hypePeriod
-  //TODO: let tagsText = event.tags
+  let hypePeriodText = "Hype period: " + event.hypePeriod.description
+  var tagsText = "Tags: "
+  for tag in event.tags {
+    tagsText = tagsText + "\(tag), "
+  }
   let eventDescriptionText = "Markdown Event Description: " + event.eventDescription.raw
   let websiteURLText = "WebsiteURL: " + (event.websiteURL ?? "null")
   let imageURLText = "ImageURL: " + (event.imageURL ?? "null")
-  //TODO: let locationText = event.location
+  var venue = "Venue: No Location", addr = "Address: No Location", long = "Longitude: No Location", lat = "Latitude: No Location"
+    if let location = event.location {
+    venue = "Venue: " + (location.venue ?? "null")
+    addr = "Address: " + (location.address ?? "null")
+    long = "Longitude: " + String(location.longitude)
+    lat = "Latitude: " + String(location.latitude)
+  }
   let facebookEventIDText = "Facebook ID: " + (event.facebookEventID ?? "null")
 
-  return El.Div.containing(
-    El.Header.containing(titleText),
-    //TODO El.Header.containing(timeText),
-    El.Header.containing(tagLineText),
-    El.Header.containing(colorText),
-    //TODO: El.Header.containing(hypeStartDateText),
-    //TODO: El.Header.containing(tagsText),
-    El.Header.containing(eventDescriptionText),
-    El.Header.containing(websiteURLText),
-    El.Header.containing(imageURLText),
-    //TODO: El.Header.containing(locationText),
-    El.Header.containing(facebookEventIDText),
+  return El.Div[Attr.className => "EventsPortal__eventContainer"].containing(
+    El.Div.containing(titleText),
+    El.Div.containing(timeText),
+    El.Div.containing(tagLineText),
+    El.Div.containing(colorText),
+    El.Div.containing(hypePeriodText),
+    El.Div.containing(tagsText),
+    El.Div.containing(eventDescriptionText),
+    El.Div.containing(websiteURLText),
+    El.Div.containing(imageURLText),
+    El.Div.containing(facebookEventIDText),
+    El.Br,
+    El.H3.containing("Location: "),
+    El.Div.containing(venue),
+    El.Div.containing(addr),
+    El.Div.containing(long),
+    El.Div.containing(lat),
+    El.Br,
     El.Div[Attr.className => "EventsPortal__divider"].containing()
   )
 }
@@ -44,7 +59,7 @@ struct EventsPage: Nodeable {
     return Page(
       title: "Events",
       content: Fragment(
-        El.Div.containing([
+        El.Div[Attr.className => "EventsPortal__mainContainer"].containing([
           El.Div.containing(eventsNumberText),
           El.H1.containing("Events in Database"),
           El.Div[Attr.className => "EventsPortal__divider"].containing(),
